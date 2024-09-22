e11dc10695032072b51af6008af35077a27c1d34
Luiz Machado <44626533+machadoluiz@users.noreply.github.com>
2024-09-22 01:56:55 +0000

    Small fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { S3 } = require('@aws-sdk/client-s3');
-const fs = require('fs');
+import { S3 } from '@aws-sdk/client-s3';
+import { createWriteStream } from 'fs';
 
 var s3 = new S3();
 
@@ -11,25 +11,25 @@ let stream = null;
 
 async function list(token) {
     let params = {
-          Bucket: SRC_BUCKET
-        };    
+        Bucket: SRC_BUCKET
+    };
 
-    if(PREFIX != "/") {
+    if (PREFIX != "/") {
         params.Prefix = PREFIX;
     }
 
-    if(!!token) {
+    if (!!token) {
         params.ContinuationToken = token;
     }
-    
-    return s3.listObjectsV2(params);   
+
+    return s3.listObjectsV2(params);
 }
 
 function write(objects) {
     objects.Contents.forEach(object => {
         let line = format(object);
         stream.write(line);
-    }); 
+    });
 }
 
 function format(object) {
@@ -38,7 +38,7 @@ function format(object) {
 
 
 function help() {
-    if(process.argv.length != 5) {
+    if (process.argv.length != 5) {
         console.log("Usage: node index.js source_bucket bucket_prefix output_file");
         process.exit(1);
     }
@@ -46,18 +46,18 @@ function help() {
 
 async function main() {
     help();
-    
+
     let hasMoreFiles = false;
     let continuationToken = null;
-    stream = fs.createWriteStream(OUTPUT_FILE, { flags: 'a' });
-    
+    stream = createWriteStream(OUTPUT_FILE, { flags: 'a' });
+
     do {
         let objects = await list(continuationToken);
         write(objects);
         hasMoreFiles = objects.IsTruncated;
-        continuationToken = (hasMoreFiles) ? objects.NextContinuationToken:null;
+        continuationToken = (hasMoreFiles) ? objects.NextContinuationToken : null;
         process.stdout.write('.');
-    } while(hasMoreFiles);
+    } while (hasMoreFiles);
 
     stream.end();
     process.stdout.write('\n');
